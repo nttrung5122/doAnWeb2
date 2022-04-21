c230f80fcd235758468ed4d565a878268bf88d6f
unknown <trihuynh02012002@gmail.com>
2022-04-21 20:54:35 +0700

    cua so tao de thi va chinh sua form dang ky

---
+++ View/_partial/form/taoCautrucde_windows.php
@@ -0,0 +1,90 @@
+<!-- Kiểm tra lại id của modal để thiết kế nút -->
+<div class="modal fade" id="" tabindex="-1" aria-labelledby="popupLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="popupLabel">Tạo đề kiểm tra</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <!-- Nội dung của pop-up -->
+            <div class="modal-body">
+                <div class="row mb-3">
+                    <label for="" class="col-sm-3 col-form-label">Mã đề</label>
+                    <div class="col">
+                        <input type="text" readonly class="form-control-plaintext" value="truyền mã đề zô đây">
+                    </div>
+                </div>
+                <hr>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-3 col-form-label">Thời gian làm bài</label>
+                    <div class="col-sm-2">
+                        <select class="form-select" aria-label="setTime" name="thoiGianlambai">
+                            <option selected disabled hidden>Thời gian</option>
+                            <option value="45">45 phút</option>
+                            <option value="60">60 phút</option>
+                            <option value="120">120 phút</option>
+                        </select>
+                    </div>
+                    <div class="col"></div>
+                </div>
+                <div class="row mb-3">
+                    <label for="ngayThi" class="col-sm-3 col-form-label">Ngày làm bài</label>
+                    <div class="col-sm-3">
+                        <input type="date" class="form-control" name="ngayThi" id="ngayThi">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-3 col-form-label">Đảo câu hỏi và đáp án</label>
+                    <div class="col">
+                        <input type="radio" class="btn-check" id="checkDaocauhoi1" name="daoCauhoi" autocomplete="off" value="true" checked>
+                        <label class="btn btn-outline-primary" for="checkDaocauhoi1">Có</label>
+                        <input type="radio" class="btn-check" id="checkDaocauhoi2" name="daoCauhoi" autocomplete="off" value="false">
+                        <label class="btn btn-outline-primary" for="checkDaocauhoi2">Không</label>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-3 col-form-label">Mật khẩu đề thi</label>
+                    <div class="col">
+                        <input type="text" class="form-control" id="mkDethi" placeholder="Mật khẩu">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-3 col-form-label">Cho phép xem đáp án</label>
+                    <div class="col">
+                        <input type="radio" class="btn-check" id="xemDapan1" name="xemDapan" autocomplete="off" value="khong" checked>
+                        <label class="btn btn-outline-primary" for="xemDapan1">Không</label>
+                        <input type="radio" class="btn-check" id="xemDapan2" name="xemDapan" autocomplete="off" value="thixong">
+                        <label class="btn btn-outline-primary" for="xemDapan2">Khi thi xong</label>
+                        <input type="radio" class="btn-check" id="xemDapan3" name="xemDapan" autocomplete="off" value="tatcathixong">
+                        <label class="btn btn-outline-primary" for="xemDapan3">Khi tất cả thi xong</label>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-3 col-form-label">Cho phép xem điểm</label>
+                    <div class="col">
+                        <input type="radio" class="btn-check" id="xemDiem1" name="xemDiem" autocomplete="off" value="khong" checked>
+                        <label class="btn btn-outline-primary" for="xemDiem1">Không</label>
+                        <input type="radio" class="btn-check" id="xemDiem2" name="xemDiem" autocomplete="off" value="thixong">
+                        <label class="btn btn-outline-primary" for="xemDiem2">Khi thi xong</label>
+                        <input type="radio" class="btn-check" id="xemDiem3" name="xemDiem" autocomplete="off" value="tatcathixong">
+                        <label class="btn btn-outline-primary" for="xemDiem3">Khi tất cả thi xong</label>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-3 col-form-label">Chia sẻ đề</label>
+                    <div class="col">
+                        <input type="radio" class="btn-check" id="chiaseDe1" name="chiaseDe" autocomplete="off" value="true" checked>
+                        <label class="btn btn-outline-primary" for="chiaseDe1">Có</label>
+                        <input type="radio" class="btn-check" id="chiaseDe2" name="chiaseDe" autocomplete="off" value="false">
+                        <label class="btn btn-outline-primary" for="chiaseDe2">Không</label>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <!-- data-bs-dismiss="modal" - đóng cửa sổ popup -->
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Tạo đề</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Thoát</button>
+            </div>
+        </div>
+    </div>
+</div>

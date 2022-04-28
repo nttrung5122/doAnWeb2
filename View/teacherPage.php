d2e1d7b90d3879d253a449e9431b209033ed1737
Zeky <nguyenphuoctoan2002@gmail.com>
2022-04-28 21:31:41 +0700

    fix teacher page, student page and update logo

--- View/teacherPage.php
+++ View/teacherPage.php
@@ -72,10 +72,10 @@ session_start();
                     success: function(data) {
                         showNotice(JSON.parse(data)['notice']);
                         if (JSON.parse(data)['status'] == 'success') {
-                            setTimeout(()=>{
+                            setTimeout(() => {
                                 window.location.reload();
                             }, 2000);
-                            
+
                         }
                     }
                 })
@@ -111,19 +111,19 @@ session_start();
             return password;
         }
 
-        function renderInfo(idClass){
+        function renderInfo(idClass) {
             $.ajax({
                 type: "POST",
                 url: "../Controller/controller.php",
                 data: {
-                    act: "getClass",    
+                    act: "getClass",
                     id: idClass,
                 },
                 success: function(data) {
-                    data=JSON.parse(data);
+                    data = JSON.parse(data);
                     $("#nameClass").html(data['tenLop']);
                     $("#infoClass").html(data['ThongTin']);
-                    $("#idClass").html("Mã lớp: "+data['maLop']);
+                    $("#idClass").html("Mã lớp: " + data['maLop']);
                     $("#soHs").html(data['soLuong']);
                     $("#idClassCurent").val(data['maLop']);
                 }
@@ -141,80 +141,29 @@ session_start();
     ?>
 
     <!-- Side Navigation -->
-    <div class="d-flex flex-column fixed-top flex-shrink-0 p-2" style="height:100vh; width: 280px; margin-top: 60.2px; background-color: #82dda5; z-index: 1;">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-            <span class="fs-3 fw-bold">Danh sách lớp</span>
-        </a>
-        <hr>
-        <ul id="class" class="nav nav-pills flex-column mb-auto">
+    <div class="d-flex flex-column fixed-top flex-shrink-0 p-2 overflow-auto" style="height:93%; width: 280px; margin-top: 60.2px; background-color: #82dda5; z-index: 1;">
+        <!-- Tính năng -->
+        <?php require("./_partial/TeacherAndStudent_Component/Sidebar.php");
+        Sidebar($teacherPage); ?>
+        <!-- Danh sách lớp -->
+        <span class="fs-3 fw-bold">Danh sách lớp</span>
+        <ul id="class" class="nav nav-pills flex-column mb-5 border-top border-dark pt-2">
             <?php
-                include "../Controller/classController.php";
-                ClassController::rederClass($_SESSION['user'][0]);
-            ?>  
-        </ul>
-        <hr>
-        <ul id="tabs" class="nav nav-pills flex-column mb-auto">
-            <li class="nav-item ">
-                <a href="#" class="nav-link active">
-                    <i class="fas fa-chart-bar"></i>
-                    Tổng Quan
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark">
-                    <i class="fas fa-users"></i>
-                    Thành Viên
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark">
-                    <i class="fas fa-book"></i>
-                    Tài Liệu
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-dark">
-                    <i class="fas fa-bullhorn"></i>
-                    Tạo Thông báo
-                </a>
-            </li>
+            include "../Controller/classController.php";
+            ClassController::rederClass($_SESSION['user'][0]);
+            ?>
         </ul>
     </div>
+
     <!-- Content -->
+    
     <div style="margin-left: 280px; margin-top: 80px;">
-        <!-- Top button -->
-        <!-- <ul class="nav nav-pills mb-auto gap-5 justify-content-center">
-            <li class="nav-item mx-5">
-                <button class="fw-bold btn btn-outline-success rounded-pill shadow">
-                    <i class="fas fa-chart-bar"></i>
-                    Tổng Quan
-                </button>
-            </li>
-            <li class="nav-item mx-5">
-                <button class="fw-bold btn btn-outline-success rounded-pill shadow">
-                    <i class="fas fa-users"></i>
-                    Thành Viên
-                </button>
-            </li>
-            <li class="nav-item mx-5">
-                <button class="fw-bold btn btn-outline-success rounded-pill shadow">
-                    <i class="fas fa-book"></i>
-                    Tài Liệu
-                </button>
-            </li>
-            <li class="nav-item mx-5">
-                <button class="fw-bold btn btn-outline-success rounded-pill shadow">
-                    <i class="fas fa-bullhorn"></i>
-                    Tạo Thông báo
-                </button>
-            </li>
-        </ul> -->
         <div class="row gap-2" style="margin-left: 0; margin-right: 0;">
             <div class="col-sm-8 mt-5 ">
                 <!-- Classroom information -->
                 <div class="container border border-3">
                     <h4 id="nameClass"></h4>
-                    <h4>Chú thích:</h4>
+                    <h4>Mô tả:</h4>
                     <!-- p for chú thích -->
                     <p class="ps-3" id="infoClass"></p>
                     <h4 id="idClass">Mã lớp:</h4>
@@ -303,37 +252,12 @@ session_start();
             </div>
         </div>
         <div class="container col-sm-3 overflow-auto text-center fixed-top bg-light" style="margin-right:0px; margin-top:70px; height:90%; z-index: 1;">
-            <!-- Announcement -->
-            <div class="border-start">
-
-                <h4>Thông báo gần nhất:</h4>
-                <p>Nguyễn Văn A đã tạo lớp</p>
-                <p>4/8/2022 lúc 07:00</p>
-            </div>
-            <hr>
-            <!-- Information -->
-            <div class="border-start">
-                <p>Thông tin</p>
-                <h4>Bài kiểm tra 1</h4>
-                <div class="ps-3 text-start row" style="margin:0; padding:0;">
-                    <div class="col-sm-6">
-                        <p>Loại</p>
-                        <p>Đã nộp</p>
-                        <p>Ngày tạo</p>
-                        <p>Chia sẻ</p>
-                    </div>
-                    <div class="col-sm-6 fw-bold">
-                        <p>Trắc nghiệm</p>
-                        <p>14/40</p>
-                        <p>1 tháng 4</p>
-                        <p><i class="fas fa-share-alt"></i></p>
-                    </div>
-                </div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique soluta quo libero minus aperiam accusamus dolorem officiis earum impedit voluptas, reiciendis ullam incidunt sed fugiat laudantium nisi architecto nihil consectetur numquam praesentium sequi quae maxime quasi. Quasi explicabo placeat voluptates! Placeat ab dolorum ipsa laboriosam molestiae? Nisi fugit beatae fugiat iure natus libero totam, unde repellendus cupiditate corporis, obcaecati veritatis repellat consequuntur voluptatibus. Eligendi consequuntur, accusamus voluptates dolore, eos maxime reiciendis, iure suscipit et qui aspernatur fuga sunt cumque alias aut dicta quam accusantium perspiciatis optio soluta labore aliquam nulla. Blanditiis, dolorum provident mollitia ipsam non facilis minus ex dicta.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique soluta quo libero minus aperiam accusamus dolorem officiis earum impedit voluptas, reiciendis ullam incidunt sed fugiat laudantium nisi architecto nihil consectetur numquam praesentium sequi quae maxime quasi. Quasi explicabo placeat voluptates! Placeat ab dolorum ipsa laboriosam molestiae? Nisi fugit beatae fugiat iure natus libero totam, unde repellendus cupiditate corporis, obcaecati veritatis repellat consequuntur voluptatibus. Eligendi consequuntur, accusamus voluptates dolore, eos maxime reiciendis, iure suscipit et qui aspernatur fuga sunt cumque alias aut dicta quam accusantium perspiciatis optio soluta labore aliquam nulla. Blanditiis, dolorum provident mollitia ipsam non facilis minus ex dicta.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique soluta quo libero minus aperiam accusamus dolorem officiis earum impedit voluptas, reiciendis ullam incidunt sed fugiat laudantium nisi architecto nihil consectetur numquam praesentium sequi quae maxime quasi. Quasi explicabo placeat voluptates! Placeat ab dolorum ipsa laboriosam molestiae? Nisi fugit beatae fugiat iure natus libero totam, unde repellendus cupiditate corporis, obcaecati veritatis repellat consequuntur voluptatibus. Eligendi consequuntur, accusamus voluptates dolore, eos maxime reiciendis, iure suscipit et qui aspernatur fuga sunt cumque alias aut dicta quam accusantium perspiciatis optio soluta labore aliquam nulla. Blanditiis, dolorum provident mollitia ipsam non facilis minus ex dicta.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique soluta quo libero minus aperiam accusamus dolorem officiis earum impedit voluptas, reiciendis ullam incidunt sed fugiat laudantium nisi architecto nihil consectetur numquam praesentium sequi quae maxime quasi. Quasi explicabo placeat voluptates! Placeat ab dolorum ipsa laboriosam molestiae? Nisi fugit beatae fugiat iure natus libero totam, unde repellendus cupiditate corporis, obcaecati veritatis repellat consequuntur voluptatibus. Eligendi consequuntur, accusamus voluptates dolore, eos maxime reiciendis, iure suscipit et qui aspernatur fuga sunt cumque alias aut dicta quam accusantium perspiciatis optio soluta labore aliquam nulla. Blanditiis, dolorum provident mollitia ipsam non facilis minus ex dicta.</p>
-            </div>
+            <?php require("./_partial/TeacherAndStudent_Component/AnnouncementAndInfo.php");
+            // Announcement
+            createAnnouncement();
+            // Information
+            createInformation();
+            ?>
         </div>
     </div>
 </body>
